6eae38b60a03c4591e57dfc9fc7cb5d406e4bac1
مهرداد تابش <mehrdadit12@gmail.com>
2023-06-20 08:41:06 -0700

    دسترسی به اتریبیوت های  تگ با js

---
+++ medium/03-access attr with js/script.js
@@ -0,0 +1,6 @@
+let h1Elem = document.getElementById("title");
+
+h1Elem.setAttribute("class","js-text");
+let liElems = document.getElementsByClassName("list-item");
+console.log(h1Elem.getAttribute("class"));
+console.log(liElems[0]);
