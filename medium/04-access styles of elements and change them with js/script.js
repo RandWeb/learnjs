4722b7810e6adb4cd5f23b6b44a290e499ad5e55
مهرداد تابش <mehrdadit12@gmail.com>
2023-06-20 08:53:38 -0700

    access style of elements and change them with js

---
+++ medium/04-access styles of elements and change them with js/script.js
@@ -0,0 +1,10 @@
+let h1Elem = document.getElementById("title");
+
+h1Elem.style.color="red";
+h1Elem.style.fontSize="40px";
+
+let liElems = document.getElementsByClassName("list-item");
+
+console.log(liElems);
+liElems[0].style.color = "red";
+liElems[0].style.fontWeight = "bold";
