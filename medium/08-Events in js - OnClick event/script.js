8dee154d13a2e2e68c18730b85281e223231a8d7
مهرداد تابش <mehrdadit12@gmail.com>
2023-08-30 23:37:04 -0700

    on click event

---
+++ medium/08-Events in js - OnClick event/script.js
@@ -0,0 +1,8 @@
+
+function changeTitleColor(){
+    //const titleElem= document.querySelector("#title");
+    const titleElem= document.getElementById("title");
+    titleElem.style.color = "blue";
+    titleElem.style.textTransform = "capitalize";
+    console.log(titleElem);
+}
