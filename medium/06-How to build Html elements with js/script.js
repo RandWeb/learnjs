336ae85e7cf93b54ec5c20a2a4c943da63c5781a
مهرداد تابش <mehrdadit12@gmail.com>
2023-08-30 22:46:24 -0700

    build elements with js

--- medium/06-How to build Html elements with js/script.js
+++ medium/06-How to build Html elements with js/script.js
@@ -1,1 +1,9 @@
- 
+
+const userName = prompt("Enter The Name: ");
+
+const newH1Elem = document.createElement("h1");
+newH1Elem.innerHTML = userName;
+newH1Elem.setAttribute("id","userName");
+newH1Elem.setAttribute("class","title");
+newH1Elem.style.color = "green";
+console.log(newH1Elem);
