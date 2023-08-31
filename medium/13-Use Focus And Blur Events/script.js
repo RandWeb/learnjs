7ce3f41848cb1cc2ccb246a1edeb949c8fafa38d
مهرداد تابش <mehrdadit12@gmail.com>
2023-08-31 05:07:39 -0700

    focus and blur events

---
+++ medium/13-Use Focus And Blur Events/script.js
@@ -0,0 +1,13 @@
+
+function onFocusUserName(){
+    console.log("user name focused");
+}
+function onBlurUserName(){
+    console.log("user name blur")
+}  
+function onFocusPassword(){
+    console.log("password focused")
+}
+function onBlurPassword(){
+    console.log("password blur")
+}               
