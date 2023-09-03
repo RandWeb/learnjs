f93b2fee22a1b1bfb2711d0e0c76fdd3c61302ab
مهرداد تابش <mehrdadit12@gmail.com>
2023-09-03 08:18:09 -0700

    diffrence between attributes and properties in dom

--- medium/27-28-29-30-do temp conversion mini project/js/script.js
+++ medium/27-28-29-30-do temp conversion mini project/js/script.js
@@ -1,4 +1,4 @@
-﻿const $=document;
+﻿const $ = document;
 let changeBtn = $.querySelector(".changeButton");
 let resetBtn = $.querySelector(".resetButton");
 let convertBtn = $.querySelector(".convertButton");
@@ -8,35 +8,41 @@ let resultElem = $.querySelector(".result");
 let inputConverter = $.getElementById("converter");
 
 
-changeBtn.addEventListener("click",()=>{
-    
-    if (inputConverter.placeholder=="°C"){
-        inputConverter.placeholder="°F";
-        $.title=" Js | °F to °C";
-        cSpan.innerHTML="°F";
-        fSpan.innerHTML="°C";
-    }else{
-        inputConverter.placeholder="°C";
-        $.title=" Js | °C to °F";
-        cSpan.innerHTML="°C";
-        fSpan.innerHTML="°F";
+changeBtn.addEventListener("click", () => {
+
+    if (inputConverter.placeholder === "°C") {
+        inputConverter.placeholder = "°F";
+        $.title = " Js | °F to °C";
+        cSpan.innerHTML = "°F";
+        fSpan.innerHTML = "°C";
+    } else {
+        inputConverter.placeholder = "°C";
+        $.title = " Js | °C to °F";
+        cSpan.innerHTML = "°C";
+        fSpan.innerHTML = "°F";
     }
 });
 
-resetBtn.addEventListener("click",()=>{
-    $.title=" Js | °C to °F";
-    cSpan.innerHTML="°C";
-    fSpan.innerHTML="°F";
-    inputConverter.value="";
-    inputConverter.placeholder="°C";
-    resultElem.innerHTML="";
+resetBtn.addEventListener("click", () => {
+    $.title = " Js | °C to °F";
+    cSpan.innerHTML = "°C";
+    fSpan.innerHTML = "°F";
+    inputConverter.value = "";
+    inputConverter.placeholder = "°C";
+    resultElem.innerHTML = "";
 
 });
 
-convertBtn.addEventListener("click",()=>{
-    if (cSpan.text=="°C"){
-        resultElem.innerHTML=`${(Number(inputConverter.value)*1.8)+32}°F`;
-    }else{
-        resultElem.innerHTML=`${(Number(inputConverter.value)-32)/8}°C`;
+convertBtn.addEventListener("click", () => {
+
+    if (inputConverter.value === "" || isNaN(Number(inputConverter.value))) {
+        resultElem.innerHTML = "invalid data";
+        return;
+    }
+    if (cSpan.text === "°C") {
+        resultElem.innerHTML = `${(Number(inputConverter.value) * 1.8) + 32}°F`;
+    } else {
+        let result = ((Number(inputConverter.value) - 32) * 5 / 9).toFixed(2);
+        resultElem.innerHTML = `${result}°C`;
     }
 });
---
+++ medium/31 - difrencess beetween attributes and properties in dom/script.js
@@ -0,0 +1,14 @@
+let h1Elem = document.getElementById("name");
+
+console.log(h1Elem.getAttribute("title")); //output : mehrdad
+console.log(h1Elem.title);
+console.log(h1Elem.getAttribute("customAttr")); //output : customAttr
+
+console.log(h1Elem.customAttr); //output : undefined
+// هر المنت  نود یک ابجکت می باشد پس می توان با دات به مقادیر آن دست یافت
+// هر اتربیوت کاستوم پراپرتی نیست و نمی توان به نقطه به آن دست یافت
+// هر اتربیوت  پیشفرض المنت، می توان با نقطه به آن دست یافت چون پراپرتی میباشد
+
+h1Elem.prop = "prop"; // add property
+console.log(h1Elem.prop);
+console.log(h1Elem.getAttribute("prop")); //output : null
