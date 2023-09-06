d4829f6ff66c96af6d6b9d2edaf7888f67e8e85a
مهرداد تابش <mehrdadit12@gmail.com>
2023-09-06 12:18:07 -0700

    debug project

---
+++ medium/40-42 Show password debuging/js/script.js
@@ -0,0 +1,23 @@
+﻿ // Functions for select elems with id & class
+ const $ = document;
+ function _id(id_name) {
+   return $.getElementById(id_name);
+ }
+
+ function _class(class_name) {
+   return $.querySelector(`.${class_name}`);
+ }
+
+ // Select Elems
+ let togglePassword = _class("toggle-password");
+ let passwordField = _id("password-field");
+
+ // Fire click event on eye icon
+ togglePassword.addEventListener("click", function () {
+   if (passwordField.type == "text") {
+     passwordField.type = "password";
+   } else {
+     passwordField.type = "text";
+   }
+     togglePassword.classList.toggle("active");
+ })
