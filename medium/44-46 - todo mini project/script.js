d9a25f125f06411c2897d43c9d30c955a03b7324
مهرداد تابش <mehrdadit12@gmail.com>
2023-09-06 13:20:39 -0700

    todo mini project

---
+++ medium/44-46 - todo mini project/script.js
@@ -0,0 +1,31 @@
+let todoUl = document.getElementById("todo");
+let inputElem = document.querySelector("input");
+
+
+function removeElem(e){
+    e.target.parentElement.remove();
+}
+inputElem.addEventListener("keydown",(e)=>{
+    
+    if (e.key==="Enter"){
+        if (e.target.value.trim().length===0){
+            alert("لطفا چیزی وارد کنید");
+        }else 
+        addTodo(e.target.value)
+        e.target.value="";
+    }
+});
+function addTodo(todo){
+    let liElem = document.createElement("li");
+    liElem.classList.add("todo-item");
+    let iElem = document.createElement("i");
+    iElem.classList.add("todo-item-remove");
+  //  iElem.setAttribute("onClick","removeElem(event)");
+    iElem.addEventListener("click",(e)=>{
+        removeElem(e);
+    })
+    iElem.innerText="X";
+    liElem.innerHTML=`${todo}`
+    liElem.appendChild(iElem);
+    todoUl.appendChild(liElem);
+}
